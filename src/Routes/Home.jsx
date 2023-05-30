d22c58a42ac263880057782bb3671e42e71a5cbe
GuyKensdale <126467015+GuyKensdale@users.noreply.github.com>
2023-05-30 14:35:10 +0100

    task 4 completed

---
+++ src/Routes/Home.jsx
@@ -0,0 +1,13 @@
+import AllArticles from "./AllArticles";
+
+const Home = () => {
+  return (
+    <>
+      <div class="body">
+        <h1>Guy's News </h1>
+        <AllArticles />
+      </div>
+    </>
+  );
+};
+export default Home;
